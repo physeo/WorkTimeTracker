8e36ef51d1a0770e8e23dec74eb4302563e47808
Bryce Graham <physeo@hotmail.com>
2019-03-16 14:00:19 -0600

    Initial

---
+++ WorkTimeTracker/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using SQLite;
+
+namespace WorkTimeTracker
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            Button btnBeginTracking = FindViewById<Button>(Resource.Id.btnBeginTracking);
+
+            btnBeginTracking.Click += (sender, e) =>
+            {
+
+            };
+        }
+    }
+}
